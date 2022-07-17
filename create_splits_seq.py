d0fd392a52e8bcbc1607001009ba62e77237d0ea
15828449889@qq.com <1528449889@qq.com>
2022-07-17 23:25:06 +0800

    增加数据 进行实验

--- create_splits_seq.py
+++ create_splits_seq.py
@@ -22,7 +22,7 @@ args = parser.parse_args()
 
 if args.task == 'task_1_tumor_vs_normal':
     args.n_classes=2
-    dataset = Generic_WSI_Classification_Dataset(csv_path = r'/home/sci/PycharmProjects/chaofan/projects/histoFL/dataset_csv/site_3.csv',  #  修改了路径 dataset_csv/tumor_vs_normal_dummy_clean.csv
+    dataset = Generic_WSI_Classification_Dataset(csv_path = r'/home/sci/PycharmProjects/chaofan/projects/histoFL/dataset_csv/site_01.csv',  #  修改了路径 dataset_csv/tumor_vs_normal_dummy_clean.csv
                             shuffle = False, 
                             seed = args.seed, 
                             print_info = True,
